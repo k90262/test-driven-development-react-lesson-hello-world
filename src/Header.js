a6a31a6860afce903109ad8f976c822e3db40177
Bill Ho <ycho.bill.lab@gmail.com>
2023-08-20 23:45:28 +0800

    [Mod3] Demo: Hello, Component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+
+function Header() {
+  return (
+    <div className="Header">
+      <p>
+        This is the header.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Header.test.js
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+import Header from './Header';
+
+it('renders without crash', () => {
+  const div = document.createElement('div');
+  createRoot(div).render(<Header />);
+});
